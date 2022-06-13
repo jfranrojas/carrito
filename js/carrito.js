66700d4ab8739f0515f8bc489f201dbcf2b68590
Francisco Rojas <em.franciscorojas.3h1@gmail.com>
2022-06-12 21:08:30 -0300

    Agregando funciones

--- js/carrito.js
+++ js/carrito.js
@@ -34,46 +34,35 @@ class Producto {
 
 let productos = [];
 //REMERAS
-productos.push(new Producto("Remera", "Remera Clásica", 2300, "L",20)); 
-productos.push(new Producto("Remera", "Remera Clásica", 2300, "L",10)); 
-productos.push(new Producto("Remera", "Remera Estampada", 4000, "L",15));   
-productos.push(new Producto("Remera", "Remera bordada", 4700, "L",5));
+productos.push(new Producto(1, "1) Remera Clásica", 2300, "L",20)); 
+productos.push(new Producto(1, "2) Remera Clásica", 2300, "L",10)); 
+productos.push(new Producto(1, "3) Remera Estampada", 4000, "L",12));
+productos.push(new Producto(1, "4) Remera bordada", 4700, "L",5));
 //GORRAS
-productos.push(new Producto("Gorra", "Gorra con logo", 2000, "M",2));
-productos.push(new Producto("Gorra", "Gorra bordada", 2300, "M",5));
-productos.push(new Producto("Gorra", "Gorra lisa", 1700, "L",10));
+productos.push(new Producto(2, "1) Gorra con logo", 2000, "M",2));
+productos.push(new Producto(2, "2) Gorra bordada", 2300, "M",5));
+productos.push(new Producto(2, "3) Gorra lisa", 1700, "L",10));
 //PANTALONES
-productos.push(new Producto("Pantalon","Pantalon jogger", 4500, 40,5));
-productos.push(new Producto("Pantalon","Pantalon jean", 5000, 42,7));
-productos.push(new Producto("Pantalon","Pantalon jean clásico", 4700, 42,15));
+productos.push(new Producto(3,"1) Pantalon jogger", 4500, 40,5));
+productos.push(new Producto(3,"2) Pantalon jean", 5000, 42,7));
+productos.push(new Producto(3,"3) Pantalon jean clásico", 4700, 42,15));
 
 
 
-const mostrarListaOrdenada = () => {
-    const array = [];
-    productos.forEach( producto => array.push(producto.modelo+" $"+producto.precio));
-    alert(prompt("Lista de precios: "+"\n"+array.join("\n")));
-};
-
-
-
-
+let categorias
 function mostrarRemeras(){
-    let remeras = productos.filter((productos)=> productos.nombre == "Remera")
-    productos = remeras
-    console.log(remeras)
+    categorias = productos.filter((productos)=> productos.nombre == 1)
+    console.log(categorias)
     mostrarListaOrdenada();  
 }
 function mostrarGorras(){
-    let gorras = productos.filter((productos)=> productos.nombre == "Gorra")
-    productos = gorras
-    console.log(gorras)
+    categorias = productos.filter((productos)=> productos.nombre == 2)
+    console.log(categorias)
     mostrarListaOrdenada();  
 }
 function mostrarPantalones(){
-    let pantalones = productos.filter((productos)=> productos.nombre == "Pantalon")
-    productos = pantalones
-    console.log(pantalones)
+    categorias = productos.filter((productos)=> productos.nombre == 3)
+    console.log(categorias)
     mostrarListaOrdenada(); 
 }
 function mostrar(){
@@ -83,8 +72,29 @@ function mostrar(){
 }
 //----------- PROCESO DE COMPRA ---------//
 
+let eleccion
+let productoFiltrado = [];
+let cantidad
+const mostrarListaOrdenada = () => {
+    let productosFiltrado = []
+    categorias.forEach( producto => productosFiltrado.push(producto.modelo+" $"+producto.precio));
+    eleccion = prompt("¿Qué te gustaría elegir?\nLista de precios: "+"\n"+productosFiltrado.join("\n"));
+    
+        if(eleccion == "" || isNaN(eleccion)) {
+        alert("Elija una opción válida")
+        mostrarListaOrdenada()
+        }
+        else{
+        cantidad = parseInt(prompt("¿Cuantos desea comprar del producto?"))
+        }
+    comprar();
+};
+
+const comprar = () => {
+        // FUNCION DE COMPRA, NO SE COMO TRAERME LA CANTIDAD DE PRODUCTOS ELEGIDOS Y MULTIPLICARLOS POR EL PRECIO
+}
+
 let producto 
-let cantidad = 0;
 let precio = 0;
 
     do{
