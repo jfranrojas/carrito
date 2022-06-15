70f0cb4f899cfe613b67508a4af143a72eb594d9
Francisco Rojas <em.franciscorojas.3h1@gmail.com>
2022-06-15 19:03:36 -0300

    Terminando compra

--- js/carrito.js
+++ js/carrito.js
@@ -25,17 +25,17 @@ class Producto {
 
 let productos = [];
 //REMERAS
-productos.push(new Producto("1) Remera Clásica", 2300,20));  
-productos.push(new Producto("2) Remera Estampada", 4000,12));
-productos.push(new Producto("3) Remera bordada", 4700,5));
+productos.push(new Producto("1) Remera Clásica", 2000,20));  
+productos.push(new Producto("2) Remera Estampada", 2500,12));
+productos.push(new Producto("3) Remera bordada", 3000,5));
 //GORRAS
 productos.push(new Producto("1) Gorra con logo", 2000,2));
-productos.push(new Producto("2) Gorra bordada", 2300,5));
-productos.push(new Producto("3) Gorra lisa", 1700,10));
+productos.push(new Producto("2) Gorra bordada", 2500,5));
+productos.push(new Producto("3) Gorra lisa", 3000,10));
 //PANTALONES
-productos.push(new Producto("1) Pantalon jogger", 200,7));
-productos.push(new Producto("2) Pantalon jean", 500,7));
-productos.push(new Producto("3) Pantalon jean clásico", 470,15));
+productos.push(new Producto("1) Pantalon jogger", 2000,7));
+productos.push(new Producto("2) Pantalon jean", 2500,7));
+productos.push(new Producto("3) Pantalon jean clásico", 3000,15));
 
 
 
@@ -60,12 +60,13 @@ function mostrar(){
     })
 }
 //----------- PROCESO DE COMPRA ---------//
-
+let total
 const sumarEnvio = () =>{
     if (total <= 4000){
         total = total + 500;
+        alert(`El total con envío es de $ ${total}. Gracias por tu compra!`)
     }else{
-        alert("El envío es gratis")
+        alert("El envío es gratis. Gracias por tu compra!")
     }
 }
 
@@ -74,26 +75,33 @@ let productoFiltrado
 let cantidad
 const mostrarListaOrdenada = () => {
     productoFiltrado = [];
-    categorias.forEach( producto => productoFiltrado.push(producto.modelo+" $"+producto.precio));
+    categorias.forEach( producto => productoFiltrado.push(producto.modelo+" $"+parseInt(producto.precio)));
     eleccion = parseInt(prompt("¿Qué te gustaría elegir?\nLista de precios: "+"\n"+productoFiltrado.join("\n")));
     console.log(eleccion)
         
     switch(eleccion){
             case 1: eleccion = productoFiltrado[0]
+                cantidad = parseInt(prompt("¿Cuantas unidades te gustaría comprar?\n Recordá que llevando mas de $4000 el envío es gratis \n \n Producto seleccionado: " +"\n"+ productoFiltrado[0]))
+                total = cantidad * 2000
+                sumarEnvio();
+                mostrarListaOrdenada();
                 break;
             case 2: eleccion = productoFiltrado[1]
+            cantidad = parseInt(prompt("¿Cuantas unidades te gustaría comprar?\n Recordá que llevando mas de $4000 el envío es gratis \n \n Producto seleccionado: " +"\n"+ productoFiltrado[1]))
+                total = cantidad * 2500
+                sumarEnvio();
+                mostrarListaOrdenada();
                 break;
             case 3: eleccion = productoFiltrado[2]
+            cantidad = parseInt(prompt("¿Cuantas unidades te gustaría comprar?\n Recordá que llevando mas de $4000 el envío es gratis \n \n Producto seleccionado: " +"\n"+ productoFiltrado[2]))
+                total = cantidad * 3000
+                sumarEnvio();
+                mostrarListaOrdenada();
                 break;
             default: alert("Ingrese un producto valido")
                 mostrarListaOrdenada();
                 break;
         }
-        
-        
-        cantidad = parseInt(prompt("¿Cuantos desea comprar del producto? "+ "\n" + eleccion ))
-        console.log(productoFiltrado[producto].modelo)
-        
     comprar();
 };
 
@@ -102,7 +110,6 @@ const comprar = () => {
 }
 
 let producto 
-let precio = 0;
 
     do{
         producto = parseInt(prompt("Bienvenido a TiendaJS, ¿Qué desea comprar? \n 1) Remeras \n 2) Gorras \n 3) Pantalones \n 4) Salir"));
